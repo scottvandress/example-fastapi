2dc38f5c0dfb560fa6fb5dde0f21c9e93375568b
Scott Van-Dress <scottvandress@gmail.com>
2022-02-08 14:21:38 -0500

    Refactored database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,13 @@
-import time
-
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -25,14 +24,15 @@ def get_db():
         db.close()
 
 
-while True:
-    try:
-        conn = psycopg2.connect(host="ec2-34-206-148-196.compute-1.amazonaws.com", database="dfj6vfmcuj4h1s",
-                                user="qtqbbhfzdsvnyi", password="9a128d521f1a484e97944a94e3f15daa0c16dca7bb1c87dcd39ee390997c9536", cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
+# while True:
+
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
+#                                 password='password123', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was succesfull!")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
